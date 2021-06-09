e1dbc142512833f089c830d487c26e4b4da1c659
Deniz Aktürk <deniz.akturk@hotmail.com>
2021-06-09 11:12:33 +0300

    Fix server.go gitignore

---
+++ server/server.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"service-frontend/config"
+)
+
+func registerRouter(w http.ResponseWriter, r *http.Request) {
+	for _, routerHolder := range config.Routes.RouterHolder {
+		for _, router := range routerHolder.Routers {
+			if router.Pattern.MatchString(r.URL.Path) {
+				router.Controller.MiddleWare(w, r)
+				router.Controller.EndpointRunner(router.Endpoint)
+			}
+		}
+	}
+}
+
+func InitServer() {
+	http.HandleFunc("/", registerRouter)
+	log.Fatal(http.ListenAndServe(":9000", nil))
+}
